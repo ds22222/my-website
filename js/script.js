27295da3965faf6289c975e507deec004d199b20
ds22222 <106778216+ds22222@users.noreply.github.com>
2023-09-10 19:32:19 +0300

    new loader

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 var menu = document.getElementById("menu");
 var action = document.getElementById("action");
 
-menu.addEventListener("click",()=>{
+menu.addEventListener("click", () => {
     hundleMenu()
 })
 
-function hundleMenu(){
-menu.classList.toggle("is-active");
-actions.classList.toggle("is-active");
+function hundleMenu() {
+    menu.classList.toggle("is-active");
+    actions.classList.toggle("is-active");
 }
+
+window.onload = function () {
+    var loader = document.querySelector(".loader");
+    loader.style.display = "none"; // إخفاء المؤشر بمجرد اكتمال التحميل
+};
